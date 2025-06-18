bb4a744d9380c8c6a1cdc74ab8e83ec40976e8c4
Rapport Sango (rapps) <rapportsango@gmail.com>
2025-06-18 12:49:42 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <CardDescription>

--- src/components/sheq-audit/audit-scheduler.tsx
+++ src/components/sheq-audit/audit-scheduler.tsx
@@ -92,7 +92,7 @@ export function AuditScheduler({ scheduledAudits, allChecklistTemplates, onSched
       <CardHeader className="flex flex-row items-center justify-between">
         <div>
           <CardTitle>Audit Program</CardTitle>
-          <CardDescription>Schedule new audits and manage upcoming ones. Select a checklist template to start.</CardDescription>
+          <CardDescription className="p-10 m-10">Schedule new audits and manage upcoming ones. Select a checklist template to start.</CardDescription>
         </div>
         <Dialog open={isModalOpen} onOpenChange={setIsModalOpen}>
           <DialogTrigger asChild>
@@ -196,3 +196,5 @@ export function AuditScheduler({ scheduledAudits, allChecklistTemplates, onSched
     </Card>
   );
 }
+
+    
