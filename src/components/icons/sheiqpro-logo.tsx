0983299ece9cfb1c652a53a87b23aa01b3a97eae
Rapport Sango (rapps) <rapportsango@gmail.com>
2025-06-27 14:23:34 +0000

    use the exact file (logo.svg) as the logo on display

--- src/components/icons/sheiqpro-logo.tsx
+++ src/components/icons/sheiqpro-logo.tsx
@@ -13,8 +13,7 @@ export const SheiqproLogo = ({ className, ...props }: React.SVGProps<SVGSVGEleme
     className={cn(className)}
     {...props}
   >
-    <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z" />
-    <path d="M9 12h6" />
-    <path d="M12 9v6" />
+    <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10" />
+    <path d="m9 12 2 2 4-4" />
   </svg>
 );
