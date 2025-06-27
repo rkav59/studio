0c7d6b4db06540c88a01293efc6aaa7373f90168
Rapport Sango (rapps) <rapportsango@gmail.com>
2025-06-27 13:57:57 +0000

    how would you upload and display a logo that actually displays without a

--- src/components/icons/sheiqpro-logo.tsx
+++ src/components/icons/sheiqpro-logo.tsx
@@ -5,7 +5,7 @@ import { cn } from '@/lib/utils';
 export const SheiqproLogo = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
   <div className={cn("relative", className)} {...props}>
     <Image
-      src="https://storage.googleapis.com/sheild-xt9s1.appspot.com/logos/sheiqpro-logo.png"
+      src="/sheiqpro-logo.png"
       alt="SHEiQpro Logo"
       fill
       style={{ objectFit: 'contain' }}
