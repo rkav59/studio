e25d1748db8a5adf7738ed09a338db733d8cdfe0
Rapport Sango (rapps) <rapportsango@gmail.com>
2025-06-27 14:01:06 +0000

    where is public/sheiqpro-logo.png

--- src/components/icons/sheiqpro-logo.tsx
+++ src/components/icons/sheiqpro-logo.tsx
@@ -1,15 +1,20 @@
-import Image from 'next/image';
-import * as React from 'react';
 import { cn } from '@/lib/utils';
+import * as React from 'react';
 
-export const SheiqproLogo = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
-  <div className={cn("relative", className)} {...props}>
-    <Image
-      src="/sheiqpro-logo.png"
-      alt="SHEiQpro Logo"
-      fill
-      style={{ objectFit: 'contain' }}
-      priority
-    />
-  </div>
+export const SheiqproLogo = ({ className, ...props }: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className={cn(className)}
+    {...props}
+  >
+    <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z" />
+    <path d="M9 12h6" />
+    <path d="M12 9v6" />
+  </svg>
 );
